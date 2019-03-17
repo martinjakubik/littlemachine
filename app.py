7ab0087e1721093ec35de2fa680623964e4ddf0c
martin jakubik <martin.jakubik@gmail.com>
2019-03-17 18:59:06 +0100

    used Guido Draheim's code to make PNGs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+from  makepng import *
 
 MAX_EXPONENT = 64
 MAX_NUMBER_OF_BOXES = 65536
@@ -26,23 +27,34 @@ def convertDecimalToBinary(decimal, pad_size):
 def printFace(boxSize, binary, printDot):
 
     column = 0
+    data = []
+    row = []
 
     for pixel in binary:
         if pixel == '1':
             block = str(u'\u2588\u2588'.encode('utf-8'))
+            row.append(255)
             sys.stdout.write(block)
         elif printDot:
             sys.stdout.write(' .')
+            row.append(0)
         else:
             sys.stdout.write('  ')
+            row.append(0)
 
         if column < (boxSize - 1):
             column = column + 1
         else:
             sys.stdout.write('\n')
+            data.append(row)
+            row = []
             column = 0
 
+    size = boxSize ** 2
     sys.stdout.write(binary + '\n\n')
+
+    with open('boxes' + str(size) + '/' + binary +  '.png', 'wb') as f:
+        f.write(makeGrayPNG(data, boxSize, boxSize))
 
 commandLineParser = argparse.ArgumentParser(description = 'generates pixel boxes')
 commandLineParser.add_argument('boxSize', type = int, help = 'the size of each box')
