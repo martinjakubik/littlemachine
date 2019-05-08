5f6a4b93693d5b6399dc4feaa6c9916f8deaabcf
martin jakubik <martin.jakubik@gmail.com>
2019-05-08 20:54:34 +0200

    displayed png

--- js/app/LabelMaker.js
+++ js/app/LabelMaker.js
@@ -4,8 +4,9 @@ requirejs(['Tools'], function (Tools) {
     'use strict';
 
     var LabelMaker = function () {
-        
         makeMainView();
+        var sBinary = '111111000';
+        addPicture(sBinary);
     };
     
     var makeMainView = function () {
@@ -15,6 +16,20 @@ requirejs(['Tools'], function (Tools) {
         oLabelMakerView.setAttribute('id', 'labelmaker');
     
         document.body.insertBefore(oLabelMakerView, null);
+    
+    };
+
+    var addPicture = function (sBinary) {
+        
+        var sFileName = `../boxes9/${sBinary}.png`;
+        var oImg = document.createElement('img');
+        Tools.setClass(oImg, 'png');
+        oImg.setAttribute('id', 'png');
+        oImg.setAttribute('src', sFileName);
+    
+        var oLabelMakerView = document.getElementById('labelmaker');
+
+        oLabelMakerView.insertBefore(oImg, null);
     
     };
 
