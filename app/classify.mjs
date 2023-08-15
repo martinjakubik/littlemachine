efc7d3386869d8122cf864c0a2cdecf51469d832
martin jakubik <martin.jakubik@gmail.com>
2023-08-15 22:45:12 +0200

    initialized allTheta

--- app/classify.mjs
+++ app/classify.mjs
@@ -1,8 +1,14 @@
+import { oneVsAll } from "./oneVsAll.mjs";
+
 const classify = function (matrixData) {
-    const allTheta = [[]];
     const m = math.size(matrixData)[0];
-    const X = math.subset(matrixData, math.index(math.range(0, m), math.range(0, 16)));
-    const y = math.squeeze(math.subset(matrixData, math.index(math.range(0, m), 16)));
+    const matrixX = math.subset(matrixData, math.index(math.range(0, m), math.range(0, 16)));
+    const aY = math.squeeze(math.subset(matrixData, math.index(math.range(0, m), 16)));
+
+    const nLabelCount = 2;
+    const nLambda = 0.1;
+
+    const allTheta = oneVsAll(matrixX, aY, nLabelCount, nLambda);
     return allTheta;
 };
 
---
+++ app/oneVsAll.mjs
@@ -0,0 +1,12 @@
+const oneVsAll = function (matrixX, aY, nLabelCount, nLambda) {
+
+    const aMatrixSize = math.size(matrixX);
+    const m = aMatrixSize[0];
+    const n = aMatrixSize[1];
+
+    const allTheta = math.zeros(m, n);
+
+    return allTheta;
+};
+
+export { oneVsAll };
