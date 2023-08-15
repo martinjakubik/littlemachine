86410e399aa3de1525021f86c53ee2b2d33034f1
martin jakubik <martin.jakubik@gmail.com>
2023-08-15 13:07:07 +0200

    extracted X, y from matrix

--- app/classify.mjs
+++ app/classify.mjs
@@ -1,6 +1,9 @@
-const classify = function (oData) {
+const classify = function (matrixData) {
     const allTheta = [[]];
+    const m = math.size(matrixData)[0];
+    const X = math.subset(matrixData, math.index(math.range(0, m), math.range(0, 16)));
+    const y = math.squeeze(math.subset(matrixData, math.index(math.range(0, m), 16)));
     return allTheta;
-}
+};
 
 export { classify };
