d3704f0c8da7a83ec2bfa95ff0aa37a1f2aad779
martin jakubik <martin.jakubik@gmail.com>
2023-09-09 18:19:40 +0200

    validated syntax of call to cost function

--- app/oneVsAll.mjs
+++ app/oneVsAll.mjs
@@ -1,11 +1,25 @@
-const oneVsAll = function (matrixX, aY, nLabelCount, nLambda) {
-    const aMatrixSize = matrixX.size();
+import { fminsearch } from "./fminsearch.mjs";
+import { lrCostFunction } from "./lrCostFunction.mjs";
+
+const oneVsAll = function (matrixX0, aY, nLabelCount, nLambda) {
+    const aMatrixSize = matrixX0.size();
     const m = aMatrixSize[0];
     const n = aMatrixSize[1];
 
-    const allTheta = math.zeros(m, n);
+    const arrayAllTheta = math.zeros(nLabelCount, n + 1);
 
-    return allTheta;
+    const matrixX1 = math.concat(math.ones(m, 1), matrixX0);
+
+    const arrayInitialTheta = math.squeeze(math.zeros(n + 1, 1));
+
+    // for c = 1:num_labels
+    // all_theta(c, :) = ...
+    //     fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+    //         initial_theta, options);
+    // end;
+    fminsearch(t => lrCostFunction(t, matrixX1, aY, nLambda), arrayInitialTheta);
+
+    return arrayAllTheta;
 };
 
 export { oneVsAll };
