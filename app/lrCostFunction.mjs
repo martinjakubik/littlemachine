ac11acf4a287ba71cae22159decde038588f2e01
martin jakubik <martin.jakubik@gmail.com>
2023-09-23 20:27:19 +0200

    continued cost function

--- app/lrCostFunction.mjs
+++ app/lrCostFunction.mjs
@@ -1,12 +1,47 @@
 import { sigmoid } from "./sigmoid.mjs";
 
 const lrCostFunction = function (matrixTheta, matrixX, arrayY, nLambda) {
+
+    /*
+    here is how cost function was imlemented in fminsearch.mjs
+
+        (y, yp) => {
+            // iterates over the array y, each element is yi
+            return y.map((yi, i) => {
+                return Math.pow((yi - yp[i]), 2);
+            }).reduce((a, b) => {
+                // sums the squares
+                return a + b;
+            });
+        }
+    */
+
     const oCost = {
         J: 0,
         grad: 0
     };
     const m = arrayY.size()[0];
     const arraySigmoids = sigmoid(math.multiply(matrixX, matrixTheta));
+
+    const fnOneMinus = function (x) {
+        return 1 - x;
+    };
+
+    // ports the follwing line from octave code
+    //      sumall = (-y' * log(sigmoids) - (1 - y)' * log(1 - sigmoids));
+    const arrayYTranspose = math.transpose(arrayY);
+    const arrayLogSigmoids = math.map(arraySigmoids, Math.log);
+    const arrayOneMinusYTranspose = math.transpose(math.map(arrayY, fnOneMinus));
+    const arrayLogOneMinusSigmoids = math.map(math.map(arraySigmoids, fnOneMinus), Math.log);
+    const nProductYTransposeByLogSigmoids = math.multiply(arrayYTranspose, arrayLogSigmoids);
+    const nProductOneMinusYTransposeByLogOneMinusSigmoids = math.multiply(arrayOneMinusYTranspose, arrayLogOneMinusSigmoids);
+    const nSumall = nProductYTransposeByLogSigmoids - nProductOneMinusYTransposeByLogOneMinusSigmoids;
+
+    // ports the follwing line from octave code
+    //      sumsquares = sum(theta(2:end) .^ 2);
+    const matrixThetaDotSquared = math.dotPow(matrixTheta, 2);
+    const nSumSquares = math.sum(matrixThetaDotSquared);
+
     return oCost;
 };
 
