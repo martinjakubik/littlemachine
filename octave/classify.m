17644261ee34cd827115a7ce609bd28b76ae3488
martin jakubik <martin.jakubik@gmail.com>
2023-09-24 13:27:39 +0200

    saved intermediate octave results to files

--- octave/classify.m
+++ octave/classify.m
@@ -29,6 +29,7 @@ fprintf('\nTraining One-vs-All Logistic Regression...\n')
 lambda = 0.1;
 [all_theta] = oneVsAll(X, y, num_labels, lambda);
 
+save all_theta.mat all_theta;
 disp(all_theta);
 
 fprintf('Program paused. Press enter to continue.\n');
@@ -36,6 +37,7 @@ pause;
 
 pred = predictOneVsAll(all_theta, X);
 
+save prediction.mat pred
 disp(pred')
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
