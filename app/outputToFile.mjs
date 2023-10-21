492b86f66419b6c5922d066c005878725b27837a
martin jakubik <martin.jakubik@gmail.com>
2023-10-21 14:43:34 +0200

    formatted js debug like octave

--- app/outputToFile.mjs
+++ app/outputToFile.mjs
@@ -1,8 +1,30 @@
-const outputToFile = function (sContent, sLabel) {
-    const sContentToOutput = JSON.stringify(sContent);
+const formatMathObject = function (oMathJSContent) {
+    const sCurrentDate = new Date().toLocaleString();
+    const sUsername = 'martin';
+    const sDataType = oMathJSContent.type;
+    const numRows = oMathJSContent._size ? oMathJSContent._size[0] : 0;
+    const numColumns = oMathJSContent._size.length === 2 ? oMathJSContent._size[1] : 1;
+    let sContentToOutput = `# Created by Little Machine, ${sCurrentDate} <${sUsername}>\n# name: sigmoids\n# type: ${sDataType}\n# rows: ${numRows}\n# columns: ${numColumns}\n`;
+    oMathJSContent._data.forEach((element, index, array) => {
+        sContentToOutput = sContentToOutput + ' ' + element;
+        if (index < array.length - 1) {
+            sContentToOutput = sContentToOutput + '\n';
+        }
+    });
+    return sContentToOutput;
+};
+
+
+const outputToFile = function (oContent, sLabel) {
+    let sContentToOutput = '';
+    if (oContent.type && oContent.type === 'DenseMatrix') {
+        sContentToOutput = formatMathObject(oContent);
+    } else {
+        sContentToOutput = JSON.stringify(oContent);
+    }
     const a = document.createElement('a');
     const sFilePrefix = sLabel ? sLabel : 'file0';
-    a.href = `data:text/plain,${sContentToOutput}`;
+    a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(sContentToOutput)}`;
     a.download = `a0-${sFilePrefix}.debug.littlemachine`;
     document.body.appendChild(a);
     a.click();
