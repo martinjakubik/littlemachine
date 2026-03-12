a0aaea3959c94e515dfc9d4b65b596889a303ce4
martin jakubik <martin.jakubik@sap.com>
2026-03-12 11:27:37 +0100

    tidy

--- app/classify.mjs
+++ app/classify.mjs
@@ -1,14 +1,23 @@
-import { oneVsAll } from "./oneVsAll.mjs";
+import { oneVsAll } from './oneVsAll.mjs';
 
 const classify = function (matrixData) {
     const m = matrixData.size()[0];
-    const matrixX = math.subset(matrixData, math.index(math.range(0, m), math.range(0, 16)));
-    const arrayY = math.squeeze(math.subset(matrixData, math.index(math.range(0, m), 16)));
+    const matrixX = math.subset(
+        matrixData,
+        math.index(math.range(0, m), math.range(0, 16)),
+    );
+    const arrayY = math.squeeze(
+        math.subset(matrixData, math.index(math.range(0, m), 16)),
+    );
 
     const nLabelCount = 2;
     const nLambda = 0.1;
 
+    const nStartTime = Date.now();
+    console.log('starting classification');
     const arrayAllTheta = oneVsAll(matrixX, arrayY, nLabelCount, nLambda);
+    const nEndTime = Date.now();
+    console.log(`finished classification; took ${nEndTime - nStartTime}`);
     return arrayAllTheta;
 };
 
--- postinstall.js
+++ postinstall.js
@@ -3,21 +3,24 @@ import * as oFs from 'fs/promises';
 const sLibPath = './app';
 
 const oMkDirOptions = {
-    recursive: true
+    recursive: true,
 };
 
-oFs.mkdir(sLibPath, oMkDirOptions).then((oResult) => {
-    console.log(oResult);
-
-    oFs.copyFile(
-        './node_modules/learnhypertext/js/index.mjs', `${sLibPath}/learnhypertext.mjs`
-    ).then((oResult) => {
+oFs.mkdir(sLibPath, oMkDirOptions)
+    .then((oResult) => {
         console.log(oResult);
-    }).catch((oError) => {
+
+        oFs.copyFile(
+            './node_modules/learnhypertext/js/index.mjs',
+            `${sLibPath}/learnhypertext.mjs`,
+        )
+            .then((oResult) => {
+                console.log(oResult);
+            })
+            .catch((oError) => {
+                console.log(oError);
+            });
+    })
+    .catch((oError) => {
         console.log(oError);
     });
-
-}).catch((oError) => {
-    console.log(oError);
-});
-
