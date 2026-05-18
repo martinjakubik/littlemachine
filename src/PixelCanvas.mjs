5c06c7735b8d86804edc20bf6d6e8447400d7efd
martin jakubik <martin.jakubik@sap.com>
2026-05-18 16:07:09 +0200

    removed unneeded getpixeloutline

--- src/PixelCanvas.mjs
+++ src/PixelCanvas.mjs
@@ -2,49 +2,13 @@ import { createCanvas } from './learnhypertext.mjs';
 
 const PIXEL_CANVAS_FILL_STYLE = 'rgba(255, 255, 255, 0)';
 
-const PIXEL_CANVAS_PIXEL_STROKE_STYLE_ON = 'rgba(255, 255, 255, 1)';
+const PIXEL_CANVAS_PIXEL_STROKE_STYLE_ON = 'rgba(170, 228, 234, 1)';
 const PIXEL_CANVAS_PIXEL_STROKE_DASH = [4, 4];
 const PIXEL_CANVAS_PIXEL_STROKE_STYLE_OFF = 'rgba(255, 255, 255, 0)';
 const PIXEL_CANVAS_PIXEL_FILL_STYLE_ON = 'rgba(71, 133, 49, 1)';
 const PIXEL_BORDER_WIDTH = 4;
 
 class PixelCanvas {
-    static getPixelOutline (x, y, nPixelSize) {
-        const nCorner = 4;
-        const borderWidth = PIXEL_BORDER_WIDTH;
-
-        return {
-            x: x,
-            y: y,
-            path: [
-                {
-                    x1: x * nPixelSize + borderWidth + nCorner,
-                    y1: y * nPixelSize + borderWidth,
-                    x2: (x + 1) * nPixelSize - borderWidth - nCorner,
-                    y2: y * nPixelSize + borderWidth,
-                },
-                {
-                    x1: (x + 1) * nPixelSize - borderWidth,
-                    y1: y * nPixelSize + borderWidth + nCorner,
-                    x2: (x + 1) * nPixelSize - borderWidth,
-                    y2: (y + 1) * nPixelSize - borderWidth - nCorner,
-                },
-                {
-                    x1: (x + 1) * nPixelSize - borderWidth - nCorner,
-                    y1: (y + 1) * nPixelSize - borderWidth,
-                    x2: x * nPixelSize + borderWidth + nCorner,
-                    y2: (y + 1) * nPixelSize - borderWidth,
-                },
-                {
-                    x1: x * nPixelSize + borderWidth,
-                    y1: (y + 1) * nPixelSize - borderWidth - nCorner,
-                    x2: x * nPixelSize + borderWidth,
-                    y2: y * nPixelSize + borderWidth + nCorner,
-                },
-            ],
-        };
-    }
-
     constructor (sCanvasId, nPixelSize, nWidth, nHeight) {
         this.canvasId = sCanvasId;
         this.pixelSize = nPixelSize;
