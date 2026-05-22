8fb93486f98e9671d721f5c9646c20e736d5a899
Martin JAKUBIK <martin.jakubik@sap.com>
2026-05-22 11:16:38 +0200

    rounded pixel corners

--- src/PixelCanvas.mjs
+++ src/PixelCanvas.mjs
@@ -32,12 +32,7 @@ class PixelCanvas {
 
         this.context = oCanvas.getContext('2d');
         this.context.fillStyle = PIXEL_CANVAS_FILL_STYLE;
-        this.context.fillRect(
-            0,
-            0,
-            nWidth,
-            nHeight
-        );
+        this.context.fillRect(0, 0, nWidth, nHeight);
 
         this.context.lineWidth = '4';
 
@@ -63,25 +58,27 @@ class PixelCanvas {
         this.context.strokeStyle = PIXEL_CANVAS_PIXEL_STROKE_STYLE_ON;
         this.context.lineWidth = '4';
         this.context.setLineDash(PIXEL_CANVAS_PIXEL_STROKE_DASH);
-        this.context.moveTo(
-            x1 * this.pixelSize,
-            y1 * this.pixelSize
-        );
-        this.context.lineTo(
-            x2 * this.pixelSize,
-            y2 * this.pixelSize
-        );
+        this.context.moveTo(x1 * this.pixelSize, y1 * this.pixelSize);
+        this.context.lineTo(x2 * this.pixelSize, y2 * this.pixelSize);
         this.context.stroke();
     }
 
     drawPixelOn (x, y) {
         this.context.fillStyle = PIXEL_CANVAS_PIXEL_FILL_STYLE_ON;
-        this.context.fillRect(
+        const oShape = this.makePixelShape();
+        this.context.moveTo(
             x * this.pixelSize + PIXEL_BORDER_WIDTH,
             y * this.pixelSize + PIXEL_BORDER_WIDTH,
-            this.pixelSize - 2 * PIXEL_BORDER_WIDTH,
-            this.pixelSize - 2 * PIXEL_BORDER_WIDTH,
         );
+        this.context.beginPath();
+        oShape.forEach((oPoint) => {
+            this.context.lineTo(
+                x * this.pixelSize + PIXEL_BORDER_WIDTH + oPoint.x,
+                y * this.pixelSize + PIXEL_BORDER_WIDTH + oPoint.y,
+            );
+        });
+        this.context.closePath();
+        this.context.fill();
     }
 
     drawPixelOff (x, y) {
@@ -91,6 +88,56 @@ class PixelCanvas {
             this.pixelSize - 2 * PIXEL_BORDER_WIDTH,
             this.pixelSize - 2 * PIXEL_BORDER_WIDTH,
         );
+    }
+
+    makePixelShape (nSize = 100) {
+        const oShapePath = [];
+        const nScale = nSize / 110;
+        const nRadius = 4;
+        const nStartOffset = 100 - nSize;
+        const nStartX = nStartOffset;
+        const nStartY = nStartOffset;
+        oShapePath.push({
+            x: (nStartX + nRadius) * nScale,
+            y: nStartY * nScale,
+        });
+        oShapePath.push({
+            x:
+                (nStartX + this.pixelSize - nRadius) * nScale -
+                PIXEL_BORDER_WIDTH / 2,
+            y: nStartY * nScale,
+        });
+        oShapePath.push({
+            x: (nStartX + this.pixelSize) * nScale - PIXEL_BORDER_WIDTH / 2,
+            y: (nStartY + nRadius) * nScale,
+        });
+        oShapePath.push({
+            x: (nStartX + this.pixelSize) * nScale - PIXEL_BORDER_WIDTH / 2,
+            y:
+                (nStartY + this.pixelSize - nRadius) * nScale -
+                PIXEL_BORDER_WIDTH / 2,
+        });
+        oShapePath.push({
+            x:
+                (nStartX + this.pixelSize - nRadius) * nScale -
+                PIXEL_BORDER_WIDTH / 2,
+            y: (nStartY + this.pixelSize) * nScale - PIXEL_BORDER_WIDTH / 2,
+        });
+        oShapePath.push({
+            x: (nStartX + nRadius) * nScale,
+            y: (nStartY + this.pixelSize) * nScale - PIXEL_BORDER_WIDTH / 2,
+        });
+        oShapePath.push({
+            x: nStartX * nScale,
+            y:
+                (nStartY + this.pixelSize - nRadius) * nScale -
+                PIXEL_BORDER_WIDTH / 2,
+        });
+        oShapePath.push({
+            x: nStartX * nScale,
+            y: (nStartY + nRadius) * nScale,
+        });
+        return oShapePath;
     }
 }
 
